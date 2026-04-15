1fec58557883afb1d7fd9a8beb05cce82b53e99b
BrianMbogo <mbogo7070@gmail.com>
2026-04-15 14:54:01 +0300

    Fix name route in router

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/name/:user_name', function (req, res) {
+  res.status(200);
+  res.set('Content-Type', 'text/html');
+  res.send(
+    '<html><body>' +
+    '<h1>Hello ' + req.params.user_name + '</h1>' +
+    '</body></html>'
+  );
+});
+
 module.exports = router;
